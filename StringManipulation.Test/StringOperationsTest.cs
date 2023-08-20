e6581d591b009c913a3fd9a23d9eac81063a32ce
LuisiitoDev <luissandoval1897@hotmail.com>
2023-08-19 22:00:47 -0600

    Test for concatenation method

---
+++ StringManipulation.Test/StringOperationsTest.cs
@@ -0,0 +1,14 @@
+﻿namespace StringManipulation.Test;
+
+public class StringOperationsTest
+{
+    [Fact]
+    public void ConcatenateStringsTest()
+    {
+        var strOperations = new StringOperations();
+
+        var result = strOperations.ConcatenateStrings("Hello", "World");
+
+        Assert.Equal("Hello World", result);
+    }
+}
